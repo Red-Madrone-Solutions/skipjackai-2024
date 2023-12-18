81ce45dd395646474d017cf0b3f65b3200e3b575
Matt Vanderpol <matt@vanderpol.net>
2023-12-18 15:40:48 -0800

    Composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rms/tailcraft',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '60a882f5cfb1e5585e761684609287938dc2317e',
+        'type' => 'theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rms/tailcraft' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '60a882f5cfb1e5585e761684609287938dc2317e',
+            'type' => 'theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
