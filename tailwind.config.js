2badd57ebea41729ed1024f7de7557463f600dbe
Matt Vanderpol <matt@vanderpol.net>
2023-12-27 16:46:49 -0800

    setup tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,45 +2,53 @@
 const fluid_type = require('tailwindcss-fluid-type');
 
 module.exports = {
-  content: [
-    './*.php',
-    './**/*.php',
-    'other-classes.txt',
-  ],
-  theme: {
-    extend: {
-      screens: {
-        'md': '782px',
-      },
-      colors: {
-        primary: {
-          DEFAULT: '#3B71CA',
-          600: '#3061AF',
-          700: '#285192',
-        }
-      },
-    },
-  },
-  corePlugins: {
-    fontSize: false
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('tailwindcss-debug-screens'),
-    fluid_type({
-        settings: {
-            fontSizeMin: 1.25,
-            fontSizeMax: 1.375,
-            ratioMin: 1.125,
-            ratioMax: 1.2,
-            screenMin: 20,
-            screenMax: 96,
-            unit: 'rem',
-            prefix: '',
-                extendValues: false,
+    content: [
+        './*.php',
+        './**/*.php',
+        'other-classes.txt',
+    ],
+    theme: {
+        extend: {
+            screens: {
+                'md': '782px',
+            },
+            margin: ({ theme }) => ({
+                'section': `clamp(${theme('width.12')}, 2.294vw + 1.041rem, ${theme('width.32')})`,
+                'layer': `clamp(${theme('width.32')}, 2.294vw + 1.041rem, ${theme('width.56')})`,
+            }),
+            padding: ({ theme }) => ({
+                'section': `clamp(${theme('width.12')}, 2.294vw + 1.041rem, ${theme('width.32')})`,
+                'layer': `clamp(${theme('width.32')}, 2.294vw + 1.041rem, ${theme('width.56')})`,
+            }),
+            colors: {
+                primary: {
+                    DEFAULT: '#3B71CA',
+                    600: '#3061AF',
+                    700: '#285192',
+                }
+            },
         },
-    }),
-  ],
+    },
+    corePlugins: {
+        fontSize: false
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('tailwindcss-debug-screens'),
+        fluid_type({
+            settings: {
+                fontSizeMin: 1.25,
+                fontSizeMax: 1.375,
+                ratioMin: 1.125,
+                ratioMax: 1.2,
+                screenMin: 20,
+                screenMax: 96,
+                unit: 'rem',
+                prefix: '',
+                extendValues: false,
+            },
+        }),
+    ],
 }
 
