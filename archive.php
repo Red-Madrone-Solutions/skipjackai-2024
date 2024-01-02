37f6c209c0121e2e0c0bbcd40bdc151cc3dbc29f
Matt Vanderpol <matt@vanderpol.net>
2024-01-02 15:08:42 -0800

    Cleanup archive and home display

--- archive.php
+++ archive.php
@@ -1,18 +1,17 @@
 <?php get_header(); ?>
 
 <div class="page-wrapper">
-    <?php if ( is_home() ) : ?>
-        <header class="mt-8">
-            <h1 class="text-3xl font-medium">Blog</h1>
-        </header>
-        <div class="grid grid-cols-3 gap-8 mt-8">
-    <?php endif; ?>
+
+    <header class="mt-8">
+        <?php the_archive_title( '<h1 class="text-3xl font-medium">', '</h1>' ); ?>
+    </header>
+    <div class="grid grid-cols-3 gap-8 mt-8">
     <?php 
         if ( have_posts() ) {
 		    while ( have_posts() ) {
 			    the_post();
                 $post_format = get_post_format();
-                if ( is_home() ) {
+                if ( is_archive() || is_home() ) {
                     get_template_part( 'template-parts/content', 'archive' );
                 } else if ( $post_format ) {
                     get_template_part( 'template-parts/content', $post_format );
@@ -22,7 +21,7 @@
             }
         }
     ?>
-    <?php if ( is_home() ) : ?></div><?php endif; ?>
+        </div>
 </div>
 
 <?php
