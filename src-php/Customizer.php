ca79b3c38a510c21464fae829257cdad89cdd7d1
Matt Vanderpol <matt@vanderpol.net>
2024-01-02 14:45:36 -0800

    Add support for default post image

--- src-php/Customizer.php
+++ src-php/Customizer.php
@@ -2,6 +2,8 @@
 
 namespace TailCraft\Theme;
 
+use WP_Customize_Image_Control;
+
 class Customizer {
     protected static $is_setup = false;
 
@@ -24,6 +26,9 @@ class Customizer {
     public function addSettings($wp_customize) : void
     {
         $this->addHeaderControls($wp_customize);
+
+        $this->addBlogSection($wp_customize);
+        $this->addBlogControls($wp_customize);
 
         $this->addFooterSection($wp_customize);
         $this->addFooterControls($wp_customize);
@@ -53,6 +58,38 @@ class Customizer {
     {
         return (bool) isset($checked) && true === $checked;
     }
+
+    protected function addBlogSection($wp_customize) : void
+    {
+        $wp_customize->add_section('tailcraft_theme_blog', [
+            'title' => 'Blog',
+            'description' => '',
+            'priority' => 100,
+        ]);
+    }
+
+    protected function addBlogControls($wp_customize) : void
+    {
+
+        $wp_customize->add_setting(
+            'tailcraft_default_post_image',
+            [
+                'default' => '',
+                'transport' => 'refresh',
+            ]
+        );
+        $wp_customize->add_control( new WP_Customize_Image_Control(
+            $wp_customize,
+            'tailcraft_default_post_image',
+            [
+                'label' => __('Default Post Image', 'tailcraft'),
+                'section' => 'tailcraft_theme_blog',
+                'settings' => 'tailcraft_default_post_image',
+                'context' => 'tailcraft_context',
+            ]
+        ));
+    }
+    
 
     protected function addFooterSection($wp_customize) : void
     {
@@ -85,7 +122,11 @@ class Customizer {
 
     }
 
-
+    public function defaultPostImage(string $classes = '') : string
+    {
+        $url = \get_theme_mod('tailcraft_default_post_image');
+        return sprintf('<img src="%s" class="%s" alt="">', esc_url($url), $classes);
+    }
 }
 
 
